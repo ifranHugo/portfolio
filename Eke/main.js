4b975786135d609ebcc65d0b8c5ad5600a54b991
ifranHugo <ifranluzbelito@gmail.com>
2023-04-17 18:58:33 -0300

    first commit

---
+++ Eke/main.js
@@ -0,0 +1,95 @@
+// ?Hamburger menu
+const d = document;
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// Event Listeners: Handling toggle event
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+//  Store color theme for future visits
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this
+  }
+}
+
+// Save user preference on load
+
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch("https://formsubmit.co/ajax/ifranluzbelito@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset();
+      })
+      .catch((err) => {
+        let message =
+          err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add(".imgAng-classBlock");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
+  });
+})(document);
